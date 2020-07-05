39be455149768a934dab24af22c4e7dd836d8925
Devin Udy <devin.udy@gmail.com>
2020-07-04 23:58:31 -0600

    First commit

---
+++ app.js
@@ -0,0 +1,8 @@
+// variables: weight in pounds, height in feet and inches, bmi
+const BMI = () => {
+    let feet = document.getElementById("feet").value;
+    let inches = document.getElementById("inches").value;
+    let weight = document.getElementById("weight").value;
+    // let bmi = weight (kg) / [height (m)]^2
+    // return bmi
+};
