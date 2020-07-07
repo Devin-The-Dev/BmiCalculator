8aa7b43883ea58a75492f228080de0b405f636ad
Devin Udy <devin.udy@gmail.com>
2020-07-06 19:13:25 -0600

    Created logic for Imperial System

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 // variables: weight in pounds, height in feet and inches, bmi
 const BMI = () => {
-    let feet = document.getElementById("feet").value;
-    let inches = document.getElementById("inches").value;
-    let weight = document.getElementById("weight").value;
-    // let bmi = weight (kg) / [height (m)]^2
-    // return bmi
+    // let feet = document.getElementById("feet").value;
+    let feet = 5;
+    // let inches = document.getElementById("inches").value;
+    let inches = 11;
+    // let pounds = document.getElementById("weight").value;
+    let pounds = 195;
+    // convert feet into inches
+    let feetToInches = feet * 12;
+    let totalInches = feetToInches + inches;
+    let bmi = 703 * (pounds / (totalInches * totalInches));
+    console.log(bmi.toFixed(1));
+    return bmi.toFixed(1);
 };
+
+document.getElementById("result").innerHTML = BMI();
